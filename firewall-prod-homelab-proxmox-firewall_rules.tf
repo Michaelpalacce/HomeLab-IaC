fdf66add34c3f4649c7eb3b5473d775accbc9a70
Stefan Genov <sgenov94@gmail.com>
2025-04-25 20:41:55 +0300

    feat(firewall): Progress

--- firewall-prod-homelab-proxmox-firewall_rules.tf
+++ firewall-prod-homelab-proxmox-firewall_rules.tf
@@ -29,7 +29,7 @@ resource "proxmox_virtual_environment_cluster_firewall_security_group" "vpn" {
 
 resource "proxmox_virtual_environment_cluster_firewall_security_group" "prox-management" {
   name    = "prox-management"
-  comment = "(Terraform) Accept communication IN only from proxmox management."
+  comment = "(Terraform) Proxmox nodes SG."
 
   rule {
     type    = "in"
@@ -56,3 +56,103 @@ resource "proxmox_virtual_environment_cluster_firewall_security_group" "prox-man
   }
 }
 
+resource "proxmox_virtual_environment_cluster_firewall_security_group" "proxy" {
+  name    = "proxy"
+  comment = "(Terraform) Special Rules for `proxy` VM. The VM is exposed, so we need to make sure it's safe."
+
+  rule {
+    type    = "in"
+    action  = "ACCEPT"
+    comment = "Allow From Management Devices"
+    source  = "+dc/${proxmox_virtual_environment_firewall_ipset.manage.id}"
+    log     = "nolog"
+  }
+
+  rule {
+    type    = "in"
+    action  = "ACCEPT"
+    proto   = "tcp"
+    source  = "+dc/${proxmox_virtual_environment_firewall_ipset.k3s.id}"
+    comment = "Allow SSH from k3s to enable backups"
+    dport   = "22"
+    log     = "nolog"
+  }
+
+  rule {
+    type    = "in"
+    action  = "ACCEPT"
+    macro   = "HTTPS"
+    source  = "+dc/${proxmox_virtual_environment_firewall_ipset.cloudflare.id}"
+    comment = "Allow HTTPS traffic through from cloudflare proxies"
+    log     = "nolog"
+  }
+
+  # rule {
+  #   type    = "in"
+  #   action  = "ACCEPT"
+  #   macro   = "HTTP"
+  #   source  = "+dc/${proxmox_virtual_environment_firewall_ipset.cloudflare.id}"
+  #   comment = "Allow HTTPS traffic through from cloudflare proxies"
+  #   log     = "nolog"
+  # }
+
+  rule {
+    type    = "in"
+    action  = "DROP"
+    source  = "dc/${proxmox_virtual_environment_firewall_alias.all.id}"
+    comment = "Drop all other incoming"
+    log     = "nolog"
+  }
+
+  # rule {
+  #   type    = "out"
+  #   action  = "ACCEPT"
+  #   macro   = "HTTPS"
+  #   dest    = "+dc/${proxmox_virtual_environment_firewall_ipset.public-lbs.id}"
+  #   comment = "Allow access to the public Load Balancers"
+  #   log     = "nolog"
+  # }
+
+  rule {
+    type    = "out"
+    action  = "ACCEPT"
+    macro   = "HTTP"
+    dest    = "+dc/${proxmox_virtual_environment_firewall_ipset.public-lbs.id}"
+    comment = "Allow access to the public Load Balancers"
+    log     = "nolog"
+  }
+}
+
+
+resource "proxmox_virtual_environment_cluster_firewall_security_group" "plex" {
+  name    = "plex"
+  comment = "(Terraform) Special firewall rules for plex."
+
+  rule {
+    type    = "in"
+    action  = "ACCEPT"
+    comment = "Allow From Management Devices"
+    source  = "+dc/${proxmox_virtual_environment_firewall_ipset.manage.id}"
+    log     = "nolog"
+  }
+
+  rule {
+    type    = "in"
+    action  = "ACCEPT"
+    comment = "Acept from internal"
+    proto   = "tcp"
+    source  = "+dc/${proxmox_virtual_environment_firewall_ipset.rfc-1918.id}"
+    dport   = "32400"
+    log     = "nolog"
+  }
+
+  rule {
+    type    = "in"
+    action  = "ACCEPT"
+    comment = "Acept from internal"
+    proto   = "udp"
+    source  = "+dc/${proxmox_virtual_environment_firewall_ipset.rfc-1918.id}"
+    dport   = "32400"
+    log     = "nolog"
+  }
+}
