73e66ee8821381c6cc0fbf3748e2631042e9ae51
Stefan Genov <sgenov94@gmail.com>
2025-04-25 18:57:37 +0300

    feat(firewall): `prox-management` migrated to terraform

--- firewall-prod-homelab-proxmox-firewall_rules.tf
+++ firewall-prod-homelab-proxmox-firewall_rules.tf
@@ -26,3 +26,33 @@ resource "proxmox_virtual_environment_cluster_firewall_security_group" "vpn" {
     log     = "nolog"
   }
 }
+
+resource "proxmox_virtual_environment_cluster_firewall_security_group" "prox-management" {
+  name    = "prox-management"
+  comment = "(Terraform) Accept communication IN only from proxmox management."
+
+  rule {
+    type    = "in"
+    action  = "ACCEPT"
+    comment = "Allow From Other Proxmox Instances"
+    source  = "+dc/${proxmox_virtual_environment_firewall_ipset.prox.id}"
+    log     = "nolog"
+  }
+
+  rule {
+    type    = "in"
+    action  = "ACCEPT"
+    comment = "Allow From Management Devices"
+    source  = "+dc/${proxmox_virtual_environment_firewall_ipset.manage.id}"
+    log     = "nolog"
+  }
+
+  rule {
+    type    = "in"
+    action  = "DROP"
+    comment = "Drop from all other places"
+    source  = "dc/${proxmox_virtual_environment_firewall_alias.all.id}"
+    log     = "nolog"
+  }
+}
+
